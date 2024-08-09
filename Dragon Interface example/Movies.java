602c024abf75353a86ce2dc01e39bba6a5e049af
GOPAL715 <cloud.gopalg@gmail.com>
2024-08-09 20:20:50 +0530

    Add files via upload

---
+++ Dragon Interface example/Movies.java
@@ -0,0 +1,14 @@
+package com;
+
+public interface Movies {
+	
+	public void MovieType(String MovieType);
+	
+	public void MovieLength(String Length);
+	
+	public void MovieHas(String Has);
+	
+	public void MovieHad(String Had);
+	
+	public void MoviesDisplay();
+}
---
+++ Dragon Interface example/Salaar.java
@@ -0,0 +1,12 @@
+package com;
+
+public interface Salaar {
+	
+	public void HeroName(String HeroName);
+	
+	
+	public void DirectorName(String DirectorName);
+	
+	public void SalaarDisplay();
+
+}
