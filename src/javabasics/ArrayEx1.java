fadb6620866ee445e9d2f000c166cbfc03a88ede
cloudgdk <163786302+cloudgdk@users.noreply.github.com>
2024-08-14 23:37:46 +0530

    Add files via upload

--- src/javabasics/ArrayEx1.java
+++ src/javabasics/ArrayEx1.java
@@ -6,21 +6,35 @@ public class ArrayEx1 {
 
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter size for storing array elements");
-		int arraLength =input.nextInt();
-		String names[] = new String[arraLength];
+		int arraLength=0;
+		String names[]; 
 		
+		String type="start";
+		
+		while(!(type.equals("exit"))) {
+			
+			System.out.println("Enter element type you want store");
+			type =input.next();
+			System.out.println("Enter how many elements want store for "+type);
+			arraLength =input.nextInt();
+			names= new String[arraLength];
+					
 		for (int i=0;i<names.length;i++) {
 			System.out.println("Enter name " +i);
 			String name = input.next();
 			names[i]= name;
 		}
+		for (int i = 0; i > 0; i--) {
+			System.out.println(names[i-1]);
+		}
+		
+		System.out.println("do want to exit");
+		type =input.next();
+		
+		}
+		
 		
 
-		// printing array
-		for (int i = 0; i < names.length; i++) {
-			System.out.print(names[i]);
-		}
 
 		
 	}
